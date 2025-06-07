e49c0e23fcfaa0b2976d585cd7adbf8cbce55716
MetaSveta <milasouls@gmail.com>
2025-06-07 15:39:44 +0200

    refactor: fix progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,7 +3,7 @@ import random
 RULE = "What number is missing in the progression?"
 
 
-def generate_round():
+def get_question_and_correct_answer():
     length = random.randint(5, 10)
     step = random.randint(1, 10)
     start = random.randint(1, 50)
@@ -13,5 +13,5 @@ def generate_round():
     correct_answer = str(progression[hidden_index])
     progression[hidden_index] = ".."
 
-    question = " ".join(str(x) for x in progression)
+    question = "Question: " + " ".join(str(x) for x in progression)
     return question, correct_answer
